2ce6507b8f1545013e5ffa8edc3e314ed6192bc1
lbarreteau <louis.barreteau@epitech.eu>
2022-05-08 22:35:09 +0200

    [ADDING] - piratage.pdf - pdf for students

---
+++ instalator.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo yum install libtool pkgconfig sqlite-devel autoconf automake openssl-devel libpcap-devel pcre-devel rfkill libnl3-devel gcc gcc-c++ ethtool hwloc-devel libcmocka-devel make file expect hostapd wpa_supplicant iw usbutils tcpdump screen zlib-devel
