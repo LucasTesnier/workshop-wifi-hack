d4c088d79d1e12e81874d9007620f56344cf5e2e
lbarreteau <louis.barreteau@epitech.eu>
2022-05-09 21:49:11 +0200

    [MODIFY] - piratage.pdf - new version of bssid

--- instalator.sh
+++ instalator.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo yum install libtool pkgconfig sqlite-devel autoconf automake openssl-devel libpcap-devel pcre-devel rfkill libnl3-devel gcc gcc-c++ ethtool hwloc-devel libcmocka-devel make file expect hostapd wpa_supplicant iw usbutils tcpdump screen zlib-devel
-airmon-ng
-iwconfig
+sudo dnf install aircrack-ng
+sudo dnf install iwconfig
